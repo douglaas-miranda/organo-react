96fd86a1b4a21b2f2ba32ae055c7dbeed1a95363
Douglas Miranda <douglas.miranda250296@gmail.com>
2023-04-25 21:54:06 -0300

    :bricks: Adicionando escutador no campotexto.

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,12 +1,20 @@
 import './CampoTexto.css';
 
 const CampoTexto = (props) => {
+
+    let valor = 'Douglas Miranda';
+
+    const aoDigitado = (e) => {
+        valor = e.target.value
+        console.log(valor)
+    }
+
     return (
         <div className="campo-texto">
             <label>
                 {props.label}
             </label>
-            <input placeholder={props.placeholder}/>
+            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder}/>
         </div>
     )
 };
