ce064e8350f2c895f42f082fd1cf191df37bc34f
Douglas Miranda <douglas.miranda250296@gmail.com>
2023-04-27 17:50:02 -0300

    :bricks: Colhendo estados dos inputs.

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -2,11 +2,9 @@ import './CampoTexto.css';
 
 const CampoTexto = (props) => {
 
-    let valor = 'Douglas Miranda';
 
     const aoDigitado = (e) => {
-        valor = e.target.value
-        console.log(valor)
+        props.aoAlterado(e.target.value);
     }
 
     return (
@@ -14,7 +12,7 @@ const CampoTexto = (props) => {
             <label>
                 {props.label}
             </label>
-            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder}/>
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder}/>
         </div>
     )
 };
