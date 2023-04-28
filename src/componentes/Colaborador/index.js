51fcc3cadd0c4dfbed3bdc8f766676b9427296ef
Douglas Miranda <douglas.miranda250296@gmail.com>
2023-04-28 19:43:37 -0300

    :bricks::lipstick: Criando componente colaborador.

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import "./Colaborador.css";
+
+const Colaborador = () => {
+  return (
+    <div className="colaborador">
+      <div className="cabecalho">
+        <img src="https://github.com/douglaas-miranda.png" alt="Douglas Miranda"/>
+      </div>
+      <div className="rodape">
+        <h4>Douglas Miranda</h4>
+        <h5>Front-End</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Colaborador;
