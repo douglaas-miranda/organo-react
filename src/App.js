f26a2ee46c6dc5471c23534bd089c2195ac356c5
Douglas Miranda <douglas.miranda250296@gmail.com>
2023-05-06 16:09:04 -0300

    ::

--- src/App.js
+++ src/App.js
@@ -3,35 +3,43 @@ import Banner from "./componentes/Banner/Banner";
 import Formulario from "./componentes/Formulario";
 import Time from "./componentes/Time";
 import Rodape from "./componentes/Rodape";
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
 
   const [times, setTimes] = useState([
     {
+      id: uuidv4(),
       nome: 'Programação',
       cor: '#D9F7E9'
     },
     {
+      id: uuidv4(),
       nome: 'Front-End',
       cor: '#E8F8FF'
     },
     {
+      id: uuidv4(),
       nome: 'Data Science',
       cor: '#F0F8E2'
     },
     {
+      id: uuidv4(),
       nome: 'Dev Ops',
       cor: '#FDE7E8'
     },
     {
+      id: uuidv4(),
       nome: 'UX e Design',
       cor: '#FAE9F5'
     },
     {
+      id: uuidv4(),
       nome: 'Mobile',
       cor: '#FFF5D9'
     },
     {
+      id: uuidv4(),
       nome: 'Inovação e Gestão',
       cor: '#FFEEDF'
     },
